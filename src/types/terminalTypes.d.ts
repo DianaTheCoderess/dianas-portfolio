d9c4f42b099faac7f01fa9c5d2d2b104957df0ff
CoderessDiana <diana@bendziula.com>
2025-03-11 17:42:14 +0100

    feat: remove unnecessary types and reduce available commands

--- src/types/terminalTypes.d.ts
+++ src/types/terminalTypes.d.ts
@@ -1,34 +1,3 @@
-import type React from "react"
-
-export type CommandType =
-  | "help"
-  | "welcome"
-  | "about"
-  | "skills"
-  | "contact"
-  | "clear"
-  | "projects"
-  | "whoami"
-  | "theme"
-  | "github"
-  | "linkedin"
-  | "neo"
-
-export interface CommandOutput {
-  type?: OutputType
-  message?: string
-  help?: string
-  content?: string
-}
-
-export interface Command {
-  input: string
-  output: CommandOutput | string | React.ReactNode
-  isError?: boolean
-  externalAction?: () => void
-  shouldClear?: boolean
-}
-
 export interface AvailableCommands {
   readonly [K in CommandType]: string
 }
